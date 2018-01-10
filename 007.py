2491e7e47e327c6505313e2fc99c9a1db5cfbccf
Michael Hagarty <michaelhagarty@gmail.com>
2018-01-10 15:31:08 +1100

    Solving problem 7.

---
+++ 007.py
@@ -0,0 +1,31 @@
+#
+# Problem 7 - 10001st prime.
+#
+# Find the 10001st prime number.
+#
+
+
+import math
+
+
+def is_prime(n):
+    """Determine whether or not n is prime."""
+
+    return all(n % x != 0 for x in range(2, math.floor(math.sqrt(n))))
+
+
+def main():
+    i = 0
+    n = 1
+
+    while i < 10001:
+        n += 1
+
+        if is_prime(n):
+            i += 1
+
+    print(n)
+
+
+if __name__ == "__main__":
+    main()
