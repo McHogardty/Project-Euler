4bf63e919898fce8db7e75d6d40d00702da61b4f
Michael Hagarty <michaelhagarty@gmail.com>
2018-01-10 17:48:32 +1100

    Improving is_prime.

--- util.py
+++ util.py
@@ -6,4 +6,8 @@ import math
 def is_prime(n):
     """Determine whether or not n is prime."""
 
+    if n < 2:
+        # A prime number is positive and has only 2 factors. 1 has one factor.
+        return False
+
     return all(n % x != 0 for x in range(2, math.floor(math.sqrt(n)) + 1))
