24e5482945b7f053cd240a6f7f334fe8b9e76135
Michael Hagarty <michaelhagarty@gmail.com>
2018-01-10 17:28:01 +1100

    Fixing error in is_prime.

--- 007.py
+++ 007.py
@@ -11,7 +11,7 @@ import math
 def is_prime(n):
     """Determine whether or not n is prime."""
 
-    return all(n % x != 0 for x in range(2, math.floor(math.sqrt(n))))
+    return all(n % x != 0 for x in range(2, math.floor(math.sqrt(n)) + 1))
 
 
 def main():
