f6408c07097a9c1d6d9241afd6378169a365c555
Michael Hagarty <michaelhagarty@gmail.com>
2018-01-12 05:42:22 +1100

    Solving problem 22.

---
+++ 022.py
@@ -0,0 +1,27 @@
+#
+# Problem 22 - Names scores
+#
+# Calculate the total of all the name scores in the file data/names.txt. The
+# name score is the multiple of the position of the name in the list with the
+# sum of the alphabetical positions of the letters in the name.
+#
+
+import string
+
+
+def name_score(name):
+    """Calculate the name score of an uppercase name."""
+
+    return sum(string.ascii_uppercase.index(x) + 1 for x in name)
+
+
+def main():
+    with open("data/names.txt", "r") as namefile:
+        names = namefile.read().replace("\"", "").split(",")
+
+    names.sort()
+    print(sum((i + 1) * name_score(name) for i, name in enumerate(names)))
+
+
+if __name__ == "__main__":
+    main()
